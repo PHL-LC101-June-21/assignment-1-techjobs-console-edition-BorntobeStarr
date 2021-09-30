c130307ca1624aebcc63954dbc6a6e515b5f8ab3
Starr <starrleer@gmail.com>
2021-09-30 12:21:03 -0400

    Fixed extra line issue -

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,15 +120,16 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
         for (HashMap<String, String> jobs: someJobs){
+            System.out.println();
             System.out.println("*****");
                 for (HashMap.Entry<String, String> i: jobs.entrySet()){
                     System.out.println(i.getKey() + ": " + i.getValue());
                 }
             System.out.println("*****");
-            System.out.println("\n");
+//            System.out.println("\n");
         }
         if (someJobs.isEmpty()){
-            System.out.println("No Results");
+            System.out.print("No Results");
         }
     }
 }
